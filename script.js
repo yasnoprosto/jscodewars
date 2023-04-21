6e86c0aee95e169554402bfa8a985ba20107cf47
Денис <denis@Deniss-MacBook-Pro.local>
2023-04-21 23:19:25 +0500

    some tasks 2104

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 //mpg to kpl converter
-/* Create an application that will display the number of kilometers per liter 
+/* 
+Create an application that will display the number of kilometers per liter 
 (output) based on the number of miles per imperial gallon (input). */
 
 function converter(mpg) {
@@ -13,4 +14,29 @@ function converter(mpg) {
 
 function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
     return laLigaGoals + copaDelReyGoals + championsLeagueGoals
+  }
+
+//return the volume of a cuboid
+  class Kata {
+    static getVolumeOfCuboid(length, width, height) {
+      return length * width * height
+    }
+  }
+/*
+Given a point in a Euclidean plane (x and y), 
+return the quadrant the point exists in: 1, 2, 3 or 4 (integer). 
+x and y are non-zero integers, therefore the given point never lies on the axes.
+*/
+function quadrant(x, y) {
+    if(x > 0 && y > 0) return 1
+    if(x < 0 && y > 0) return 2
+    if(x < 0 && y < 0) return 3
+    if(x > 0 && y < 0) return 4
+  }
+
+  /* Define a function that counts how many more (or less) pieces of toast you need in the toasters.
+   Even though you need more or less, the number will still be positive, not negative. */
+
+   function sixToast(num) {
+    return Math.abs(6 - num)
   }
