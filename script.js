24751b6b10255b2c1be96b4030318fb7c14374a3
Денис <denis@Deniss-MacBook-Pro.local>
2023-04-21 22:29:51 +0500

    Grasshopper Function

--- script.js
+++ script.js
@@ -7,3 +7,10 @@ function converter(mpg) {
   kpl = mpg * (1.609344 / 4.54609188);
   return Number(kpl.toFixed(2));
 }
+
+//Grasshopper
+//Complete the function to return his total number of goals in all three leagues.
+
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+    return laLigaGoals + copaDelReyGoals + championsLeagueGoals
+  }
