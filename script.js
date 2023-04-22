b0f156add75e2a1c7a6b98333bba61c7307d60ce
Денис <denis@Deniss-MacBook-Pro.local>
2023-04-22 07:58:05 +0500

    2204 codewars tasks

--- script.js
+++ script.js
@@ -39,4 +39,21 @@ function quadrant(x, y) {
 
    function sixToast(num) {
     return Math.abs(6 - num)
+   }
+// Cube Checker
+
+function cubeChecker(volume, side) {
+    if (volume <= 0 || side <= 0) {
+      return false;
+    }
+
+    let length = volume / (side ** 2);
+    let width = length;
+    let height = length;
+    
+    if (length === side && width === side && height === side) {
+      return true;
+    } else {
+      return false;
+    }
   }
