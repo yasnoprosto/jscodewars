1ab66a9c7274d84f33857706f47d1c00a5e4cd26
Денис <denis@Deniss-MacBook-Pro.local>
2023-04-24 14:10:08 +0500

    2404

--- 2404.js
+++ 2404.js
@@ -4,3 +4,17 @@ function shortenToDate(longDate) {
     let commaIndex = longDate.indexOf(",");
     return longDate.slice(0, commaIndex)
   }
+/* 
+Create a function that accepts a string and a single character, 
+and returns an integer of the count of occurrences the 2nd argument is found in the first one.
+*/
+function strCount(str, letter){  
+    let counter = 0;
+    let arrayOfLetters = str.split('');
+
+    for(let l of arrayOfLetters) {
+      if(letter === l) counter++;
+    }
+    
+    return counter
+  }
