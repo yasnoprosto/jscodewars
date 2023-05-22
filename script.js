0fec17d6bb12cb263ec7a0bbc58f1625230b2776
Denis Pochinskiy <ircxenon1@gmail.com>
2023-05-22 10:40:15 +0500

    2205 upd

--- script.js
+++ script.js
@@ -12,32 +12,46 @@ function converter(mpg) {
 //Grasshopper
 //Complete the function to return his total number of goals in all three leagues.
 
-function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
-    return laLigaGoals + copaDelReyGoals + championsLeagueGoals
-  }
+function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+  return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+}
 
 //return the volume of a cuboid
-  class Kata {
-    static getVolumeOfCuboid(length, width, height) {
-      return length * width * height
-    }
+class Kata {
+  static getVolumeOfCuboid(length, width, height) {
+    return length * width * height;
   }
+}
 /*
 Given a point in a Euclidean plane (x and y), 
 return the quadrant the point exists in: 1, 2, 3 or 4 (integer). 
 x and y are non-zero integers, therefore the given point never lies on the axes.
 */
 function quadrant(x, y) {
-    if(x > 0 && y > 0) return 1
-    if(x < 0 && y > 0) return 2
-    if(x < 0 && y < 0) return 3
-    if(x > 0 && y < 0) return 4
-  }
+  if (x > 0 && y > 0) return 1;
+  if (x < 0 && y > 0) return 2;
+  if (x < 0 && y < 0) return 3;
+  if (x > 0 && y < 0) return 4;
+}
 
-  /* Define a function that counts how many more (or less) pieces of toast you need in the toasters.
+/* Define a function that counts how many more (or less) pieces of toast you need in the toasters.
    Even though you need more or less, the number will still be positive, not negative. */
 
-   function sixToast(num) {
-    return Math.abs(6 - num)
-   }
+function sixToast(num) {
+  return Math.abs(6 - num);
+}
 
+// Adding li with each element of array
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const btn = document.querySelector("#btn");
+const ul = document.querySelector("#ul");
+
+btn.addEventListener("click", () => {
+  for (const i of arr) {
+    const li = document.createElement("li");
+    const element = i;
+    li.appendChild(document.createTextNode(element));
+    ul.appendChild(li);
+  }
+});
