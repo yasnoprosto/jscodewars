8670d2303be9e76f9e443f737b69633a36b0e1c4
Denis Pochinskiy <ircxenon1@gmail.com>
2023-05-23 23:09:59 +0500

    added count average function

---
+++ tasks/2305.ts
@@ -0,0 +1,9 @@
+// count average
+export function findAverage(array: number[]): number {
+ if(array.length === 0 ) return 0
+  let sum: number = 0;
+  for(let i of array) {
+    sum += i
+  }
+  return sum / array.length;
+}
